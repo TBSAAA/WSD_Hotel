32644a88e16bbb07c3bcce27940e1198cb6cdcfc
Jack <huanghaoranhoo@gmail.com>
2021-09-20 16:53:27 +1000

    add Identity package

---
+++ Hotel19966292/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hotel19966292.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Hotel19966292.Areas.Identity.IdentityHostingStartup))]
+namespace Hotel19966292.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
